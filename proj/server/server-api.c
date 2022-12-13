ea82e54782d20bc188b257554bd8e8d06a2b0952
nunodev <nuno.dev96@gmail.com>
2022-12-13 13:15:53 +0000

    added

--- proj/server/server-api.c
+++ proj/server/server-api.c
@@ -158,6 +158,26 @@ char *getLineFromFile(char *filename, int lineNum)
     return NULL;
 }
 
+// getNumberOfLinesFromFile
+int getNumberOfLinesFromFile(char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        return -1;
+    }
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int i = 0;
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        i++;
+    }
+    fclose(file);
+    return i;
+}
+
 // getGameWordFromFile: returns the first word from the first line of /games/PLID.txt
 char *getGameWordFromFile(char *filename)
 {
@@ -183,6 +203,51 @@ char *getGameHintFromFile(char *filename)
     char *word = strtok(line, " ");
     char *hint = strtok(NULL, " ");
     return hint;
+}
+
+// getCorrectPlayPositions: returns string of all positions where the char played is correct
+char *getCorrectPlayPositions(char *word, char *guess)
+{
+    char *positions = malloc(strlen(word) + 1);
+    int i;
+    int j = 0;
+    for (i = 0; i < strlen(word); i++)
+    {
+        if (word[i] == guess[0])
+        {
+            positions[j] = i + '0';
+            j++;
+        }
+    }
+    positions[j] = '\0';
+    return positions;
+}
+
+// hasBeenPlayedBefore: iterates through filename and checks if line has string already
+int hasBeenPlayedBefore(char *filename, char *type, char *string)
+{
+    // type can be "play" or "guess"
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        return -1;
+    }
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        char *token1 = strtok(line, " ");
+        char *token2 = strtok(NULL, " ");
+        // if type == token1, check if token2 == string
+        if (!strcmp(type, token1) && !strcmp(string, token2))
+        {
+            fclose(file);
+            return 1;
+        }
+    }
+    fclose(file);
+    return 0;
 }
 
 char *processServerStart(char **tokenList, int numTokens)
@@ -250,19 +315,61 @@ char *processServerPlay(char **tokenList, int numTokens)
     // tokenList = PLG PLID letter trial
     if (numTokens != 4)
     {
-        return getServerReplyUDP(PLAY, "NOK");
+        return getServerReplyUDP(PLAY, "ERR");
     }
     else if (!isValidPLID(tokenList[1]))
     {
-        return getServerReplyUDP(PLAY, "NOK");
+        return getServerReplyUDP(PLAY, "ERR");
     }
     else if (!isValidPlay(tokenList[2]))
     {
-        return getServerReplyUDP(PLAY, "NOK");
+        return getServerReplyUDP(PLAY, "ERR");
     }
     else if (!isValidTrial(tokenList[3]))
     {
+        return getServerReplyUDP(PLAY, "ERR");
+    }
+    // check if games/PLID.txt exists
+    char *filename = malloc(strlen(tokenList[1]) + 7);
+    sprintf(filename, "games/%s.txt", tokenList[1]);
+    if (access(filename, F_OK) != -1)
+    {
+        free(filename);
+        return getServerReplyUDP(START, "ERR");
+    }
+    // check if tokenList[3] is equal to the number of lines in games/PLID.txt
+    int numLines = getNumberOfLinesFromFile(filename);
+    if (numLines == -1)
+    {
+        free(filename);
+        return getServerReplyUDP(PLAY, "ERR");
+    }
+    else if (numLines != atoi(tokenList[3]))
+    {
+        free(filename);
+        return getServerReplyUDP(PLAY, "INV");
+    }
+    // get word from first line of games/PLID.txt
+    char *gameWord = getGameWordFromFile(filename);
+    if (gameWord == NULL)
+    {
+        free(filename);
+        return getServerReplyUDP(PLAY, "ERR");
+    }
+    // check if has been played before
+    if (hasBeenPlayedBefore(filename, "play", tokenList[2]))
+    {
+        free(filename);
+        return getServerReplyUDP(PLAY, "DUP");
+    }
+    // get correct positions of tokenList[2] in gameWord
+    char *correctPositions = getCorrectPlayPositions(gameWord, tokenList[2]);
+    if (correctPositions == NULL)
+    {
+        free(filename);
         return getServerReplyUDP(PLAY, "NOK");
     }
+
+    free(filename);
     return getServerReplyUDP(PLAY, "OK");
 }
